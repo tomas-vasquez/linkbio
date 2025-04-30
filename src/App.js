905275699b683779c9cadeba42747389f69d7e5a
tomas-vasquez <tomasdetloging@gmail.com>
2025-04-29 22:04:50 -0400

    ynzn

--- src/App.js
+++ src/App.js
@@ -16,15 +16,55 @@ function App() {
                 return <MyButton key={index} text={item.text} url={item.url} />
               } else if (item.type === 'separator') {
                 return (
-                  <p class="my-4 text-center" key={index}>
+                  <p className="my-4 text-center" key={index}>
                     {item.text}
                   </p>
                 )
               }
               return null
             })}
+
+            <h2 className="mt-5">Accesos rápidos a apps y configuraciones</h2>
+
+            <a
+              href="intent://#Intent;package=com.android.chrome;end"
+              className="button d-block my-2"
+            >
+              Abrir Google Chrome
+            </a>
+            <a
+              href="intent://#Intent;action=android.settings.SETTINGS;end"
+              className="button d-block my-2"
+            >
+              Abrir Configuración General
+            </a>
+            <a
+              href="intent://#Intent;action=android.settings.WIFI_SETTINGS;end"
+              className="button d-block my-2"
+            >
+              Abrir Configuración de Wi-Fi
+            </a>
+            <a
+              href="intent://#Intent;action=android.settings.BLUETOOTH_SETTINGS;end"
+              className="button d-block my-2"
+            >
+              Abrir Bluetooth
+            </a>
+            <a
+              href="intent://#Intent;action=android.settings.DISPLAY_SETTINGS;end"
+              className="button d-block my-2"
+            >
+              Abrir Configuración de Pantalla
+            </a>
+            <a
+              href="intent://#Intent;action=android.intent.action.MAIN;category=android.intent.category.HOME;end"
+              className="button d-block my-2"
+            >
+              Ir al Inicio (Home)
+            </a>
+
             <footer className="mt-5 text-center">
-              <small> TomyCell® {new Date().getFullYear()}</small>
+              <small>TomyCell® {new Date().getFullYear()}</small>
             </footer>
           </div>
         </div>
